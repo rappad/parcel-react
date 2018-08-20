88e28634bbc016073f486cc62d2ec611640de9db
was.li <wasuwat.se@gmail.com>
2018-08-20 18:55:32 +0700

    [🎉] init

---
+++ main.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './main.scss'
+
+const App = () => (
+  <main>
+    <h1>Parcel x React</h1>
+    <section>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, blanditiis nihil aperiam veniam modi expedita reiciendis
+      accusantium fugit, voluptatibus possimus error impedit labore repudiandae quaerat similique aliquid sit obcaecati, voluptas
+      corporis dolor asperiores nisi. Quos error placeat sed reiciendis aliquam recusandae nemo, consequuntur blanditiis suscipit
+      est, inventore corrupti distinctio, velit mollitia voluptas! Molestiae enim obcaecati dolores blanditiis sit error et
+      exercitationem beatae, quod dolore accusamus ab magni minus voluptatum optio temporibus officiis recusandae in! Voluptatum
+      amet maiores, cumque soluta tenetur consequatur nostrum placeat dolorum aut eaque commodi sunt nulla modi, provident
+      sapiente dolor id necessitatibus tempore explicabo eveniet deleniti ipsum? Lorem ipsum, dolor sit amet consectetur
+      adipisicing elit.
+    </section>
+    <section>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi temporibus rerum earum beatae eius, at magni nisi assumenda
+      repellat? Nisi ea voluptates quasi est illo voluptatibus consequatur aut veniam veritatis inventore, sequi ex ullam labore
+      perferendis nobis molestiae, quod sed iusto! Cupiditate neque esse enim ipsum, dolorem earum. Labore rerum vero id dolor!
+      Beatae, architecto delectus laboriosam dolor debitis nulla! Error, eligendi odio vel, ab reiciendis tenetur velit ut quas in
+      iure animi tempora saepe! Delectus magni maxime, dignissimos nulla ex consectetur ipsa veritatis perferendis quam asperiores
+      repellat! Esse laudantium repellendus voluptas quibusdam quia enim voluptatum. Obcaecati neque culpa illo modi ut. Pariatur
+      dignissimos voluptate molestiae aspernatur minima voluptates? Tempore, laborum non minus exercitationem praesentium ducimus
+      earum quia provident quae labore asperiores totam expedita est quod explicabo aperiam alias dolorum. Veritatis odio, quaerat
+      tempora accusamus nihil praesentium minima libero impedit neque culpa fuga! Vel doloremque quod saepe libero quis, quaerat
+      mollitia fugiat molestiae beatae maiores a est nulla obcaecati esse expedita ex laboriosam ducimus tempore aliquam modi
+      repudiandae architecto fuga! Nihil eum ullam iusto totam nemo. Sit repudiandae laboriosam est nesciunt nobis, pariatur,
+      numquam molestiae eius voluptates architecto voluptatum culpa error quam molestias consequuntur nihil laudantium enim
+      accusamus debitis sequi.
+    </section>
+  </main>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
