4742d309a4f9ca5af37770e29fdef4926d45e3db
was.li <wasuwat.se@gmail.com>
2018-08-21 11:36:41 +0700

    [💄] adjust fonts & responsive

--- src/main.js
+++ src/main.js
@@ -9,11 +9,7 @@ const App = () => (
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, blanditiis nihil aperiam veniam modi expedita reiciendis
       accusantium fugit, voluptatibus possimus error impedit labore repudiandae quaerat similique aliquid sit obcaecati, voluptas
       corporis dolor asperiores nisi. Quos error placeat sed reiciendis aliquam recusandae nemo, consequuntur blanditiis suscipit
-      est, inventore corrupti distinctio, velit mollitia voluptas! Molestiae enim obcaecati dolores blanditiis sit error et
-      exercitationem beatae, quod dolore accusamus ab magni minus voluptatum optio temporibus officiis recusandae in! Voluptatum
-      amet maiores, cumque soluta tenetur consequatur nostrum placeat dolorum aut eaque commodi sunt nulla modi, provident
-      sapiente dolor id necessitatibus tempore explicabo eveniet deleniti ipsum? Lorem ipsum, dolor sit amet consectetur
-      adipisicing elit.
+      est.
     </section>
     <section>
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi temporibus rerum earum beatae eius, at magni nisi assumenda
@@ -24,11 +20,7 @@ const App = () => (
       repellat! Esse laudantium repellendus voluptas quibusdam quia enim voluptatum. Obcaecati neque culpa illo modi ut. Pariatur
       dignissimos voluptate molestiae aspernatur minima voluptates? Tempore, laborum non minus exercitationem praesentium ducimus
       earum quia provident quae labore asperiores totam expedita est quod explicabo aperiam alias dolorum. Veritatis odio, quaerat
-      tempora accusamus nihil praesentium minima libero impedit neque culpa fuga! Vel doloremque quod saepe libero quis, quaerat
-      mollitia fugiat molestiae beatae maiores a est nulla obcaecati esse expedita ex laboriosam ducimus tempore aliquam modi
-      repudiandae architecto fuga! Nihil eum ullam iusto totam nemo. Sit repudiandae laboriosam est nesciunt nobis, pariatur,
-      numquam molestiae eius voluptates architecto voluptatum culpa error quam molestias consequuntur nihil laudantium enim
-      accusamus debitis sequi.
+      tempora accusamus nihil
     </section>
   </main>
 )
