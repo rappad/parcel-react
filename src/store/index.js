d0b3e5b89ca66227557527ca9af8b7453c05a843
was.li <wasuwat.se@gmail.com>
2018-09-29 14:37:52 +0700

    [🔧] config logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
+import logger from 'redux-logger'
+
 import rootReducer from '../reducer'
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(rootReducer, applyMiddleware(logger))
 
 export default store
